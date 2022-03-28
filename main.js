407892ef5b6d0d2d9a80833edea49f0023c57df9
Manjuneoito <manju@neoito.com>
2022-03-28 12:44:15 +0530

    Ui Task Completed

--- main.js
+++ main.js
@@ -7,3 +7,40 @@ for (var i = 0; i < btns.length; i++) {
   this.className += " active";
   });
 }
+
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("myAction");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction1() {
+  var x = document.getElementById("myAction1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction2() {
+  var x = document.getElementById("myAction2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction3() {
+  var x = document.getElementById("myAction3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
